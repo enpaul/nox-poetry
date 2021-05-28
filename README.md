84177cbfb10df89f3d492596151d861ee8662d85
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2021-05-28 00:11:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# nox-poetry
+
+A plugin for Nox that lets you install test environment dependencies from the Poetry
+lockfile
---
+++ nox_poetry/__about__.py
@@ -0,0 +1,7 @@
+# pylint: disable=missing-docstring
+__title__ = "nox-poetry"
+__summary__ = "A plugin for Nox that lets you install test environment dependencies from the Poetry lockfile"
+__version__ = "0.8.0"
+__url__ = "https://github.com/enpaul/nox-poetry/"
+__license__ = "MIT"
+__authors__ = ["Ethan Paul <24588726+enpaul@users.noreply.github.com>"]
